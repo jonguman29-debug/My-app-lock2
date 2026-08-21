a19cb30d918a1062270508fe7c495dc614c2d58d
jonguman29-debug <jon.gu.man.29@gmail.com>
2026-08-21 12:51:04 +0300

    Update LockService.java

--- app/src/main/java/com/yohannes/myapplock2/LockService.java
+++ app/src/main/java/com/yohannes/myapplock2/LockService.java
@@ -1,1 +1,19 @@
 
+package com.yohannes.myapplock2;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class LockService extends Service {
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
